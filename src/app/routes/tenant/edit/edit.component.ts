c6821add93bbb14673983480e5bce49c4a98e8fc
pengcl <3559996@qq.com>
2019-03-20 11:44:44 +0800

    更新商品编辑router

--- src/app/routes/tenant/edit/edit.component.ts
+++ src/app/routes/tenant/edit/edit.component.ts
@@ -3,9 +3,11 @@ import {
   OnInit
 } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {LocationStrategy} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 
 import {TenantInfoEditDto, TenantServiceProxy} from '@shared/service-proxies/service-proxies';
+import {getIndex} from '@shared/utils/utils';
 
 @Component({
   selector: 'app-tenant-edit',
@@ -14,17 +16,35 @@ import {TenantInfoEditDto, TenantServiceProxy} from '@shared/service-proxies/ser
 })
 export class TenantEditComponent implements OnInit {
   id = this.route.snapshot.params['id'];
-  tenantForm: FormGroup;
+  tenant;
+  features;
 
   constructor(private route: ActivatedRoute,
+              private location: LocationStrategy,
               private tenantSvc: TenantServiceProxy) {
   }
 
   ngOnInit() {
     if (this.id !== '0') {
+      const features = [];
       this.tenantSvc.getTenantForEdit(this.id).subscribe(res => {
+        this.tenant = res.tenant;
         console.log(res);
+        res.features.features.forEach(item => {
+          item['value'] = res.features.featureValues[getIndex(res.features.featureValues, 'name', item.name)].value;
+          features.push(item);
+        });
+        this.features = features;
+        console.log(this.features);
       });
     }
+  }
+
+  submit() {
+    console.log('submit');
+  }
+
+  cancel() {
+    this.location.back();
   }
 }
