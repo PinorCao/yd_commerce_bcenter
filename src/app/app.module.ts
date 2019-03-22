99a74ebceb0806d27dcbe37d8c9363faf2311a28
Victor_Lo <Victor_Lo@vapps.hk>
2019-03-22 19:08:10 +0800

    整理

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,38 @@
-import {NgModule, Injector, LOCALE_ID, APP_INITIALIZER} from '@angular/core';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule, Injector, LOCALE_ID, APP_INITIALIZER } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as _ from 'lodash';
 
 // #region default language
 // 参考：https://ng-alain.com/docs/i18n
-import {default as ngLang} from '@angular/common/locales/zh';
-import {NZ_I18N, zh_CN as zorroLang} from 'ng-zorro-antd';
-import {DELON_LOCALE, zh_CN as delonLang} from '@delon/theme';
-import {JWTInterceptor} from '@delon/auth';
+import { default as ngLang } from '@angular/common/locales/zh';
+import { NZ_I18N, zh_CN as zorroLang } from 'ng-zorro-antd';
+import { DELON_LOCALE, zh_CN as delonLang } from '@delon/theme';
+import { JWTInterceptor } from '@delon/auth';
 
 const LANG = {
   abbr: 'zh',
   ng: ngLang,
   zorro: zorroLang,
-  delon: delonLang
+  delon: delonLang,
 };
 // register angular
-import {registerLocaleData} from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(LANG.ng, LANG.abbr);
 const LANG_PROVIDES = [
-  {provide: LOCALE_ID, useValue: LANG.abbr},
-  {provide: NZ_I18N, useValue: LANG.zorro},
-  {provide: DELON_LOCALE, useValue: LANG.delon}
+  { provide: LOCALE_ID, useValue: LANG.abbr },
+  { provide: NZ_I18N, useValue: LANG.zorro },
+  { provide: DELON_LOCALE, useValue: LANG.delon },
 ];
 // #endregion
 
 // #region i18n services
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {MenuService, ALAIN_I18N_TOKEN} from '@delon/theme';
-import {I18NService} from '@core/i18n/i18n.service';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MenuService, ALAIN_I18N_TOKEN } from '@delon/theme';
+import { I18NService } from '@core/i18n/i18n.service';
 
 // 加载i18n语言文件
 export function I18nHttpLoaderFactory(http: HttpClient) {
@@ -44,13 +44,13 @@ const I18NSERVICE_MODULES = [
     loader: {
       provide: TranslateLoader,
       useFactory: I18nHttpLoaderFactory,
-      deps: [HttpClient]
-    }
-  })
+      deps: [HttpClient],
+    },
+  }),
 ];
 
 const I18NSERVICE_PROVIDES = [
-  {provide: ALAIN_I18N_TOKEN, useClass: I18NService, multi: false}
+  { provide: ALAIN_I18N_TOKEN, useClass: I18NService, multi: false },
 ];
 
 // #endregion
@@ -62,38 +62,38 @@ const GLOBAL_THIRD_MODULES = [];
 // #endregion
 
 // #region JSON Schema form (using @delon/form)
-import {JsonSchemaModule} from '@shared/json-schema/json-schema.module';
+import { JsonSchemaModule } from '@shared/json-schema/json-schema.module';
 
 const FORM_MODULES = [JsonSchemaModule];
 // #endregion
 
-// #region Http Interceptors
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {SimpleInterceptor} from '@delon/auth';
-import {DefaultInterceptor} from '@core/net/default.interceptor';
+// #region Http Interceptorsa
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SimpleInterceptor } from '@delon/auth';
+import { DefaultInterceptor } from '@core/net/default.interceptor';
 
-import {AbpHttpInterceptor} from '@abp/abpHttpInterceptor';
+import { AbpHttpInterceptor } from '@abp/abpHttpInterceptor';
 
 const INTERCEPTOR_PROVIDES = [
   /*{ provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true },*/
-  {provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true},
-  {provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true},
-  {provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true}
+  { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true },
   /*{ provide: API_BASE_URL, useFactory: getRemoteServiceBaseUrl }*/
 ];
 // #endregion
 
 // #region Startup Service
-import {StartupService} from '@core/startup/startup.service';
-import {AppPreBootstrap} from '../AppPreBootstrap';
-import {AppSessionService} from '@shared/session/app-session.service';
-import {AbpSharedModule} from '@shared/abpshared.module';
-import {ServiceProxyModule} from '@shared/service-proxies/service-proxy.module';
+import { StartupService } from '@core/startup/startup.service';
+import { AppPreBootstrap } from '../AppPreBootstrap';
+import { AppSessionService } from '@shared/session/app-session.service';
+import { AbpSharedModule } from '@shared/abpshared.module';
+import { ServiceProxyModule } from '@shared/service-proxies/service-proxy.module';
 
 export function StartupServiceFactory(
   injector: Injector,
-  startupService: StartupService
+  startupService: StartupService,
 ): Function {
   return () =>
     startupService
@@ -109,7 +109,7 @@ export function StartupServiceFactory(
           abp.event.trigger('abp.dynamicScriptsInitialized');
 
           const appSessionService: AppSessionService = injector.get(
-            AppSessionService
+            AppSessionService,
           );
 
           appSessionService.init().then(
@@ -118,7 +118,7 @@ export function StartupServiceFactory(
             },
             err => {
               reject(err);
-            }
+            },
           );
         });
       });
@@ -162,19 +162,19 @@ const APPINIT_PROVIDES = [
     provide: APP_INITIALIZER,
     useFactory: StartupServiceFactory,
     deps: [Injector, StartupService],
-    multi: true
-  }
+    multi: true,
+  },
 ];
 // #endregion
 
-import {DelonModule} from './delon.module';
-import {CoreModule} from '@core/core.module';
-import {SharedModule} from '@shared/shared.module';
-import {AppComponent} from './app.component';
-import {RoutesModule} from './routes/routes.module';
-import {LayoutModule} from './layout/layout.module';
+import { DelonModule } from './delon.module';
+import { CoreModule } from '@core/core.module';
+import { SharedModule } from '@shared/shared.module';
+import { AppComponent } from './app.component';
+import { RoutesModule } from './routes/routes.module';
+import { LayoutModule } from './layout/layout.module';
 
-import {AbpModule} from 'abp-ng2-module/dist';
+import { AbpModule } from 'abp-ng2-module/dist';
 
 @NgModule({
   declarations: [AppComponent],
@@ -192,15 +192,15 @@ import {AbpModule} from 'abp-ng2-module/dist';
     RoutesModule,
     ...I18NSERVICE_MODULES,
     ...GLOBAL_THIRD_MODULES,
-    ...FORM_MODULES
+    ...FORM_MODULES,
   ],
   providers: [
     ...LANG_PROVIDES,
     ...INTERCEPTOR_PROVIDES,
     ...I18NSERVICE_PROVIDES,
-    ...APPINIT_PROVIDES
+    ...APPINIT_PROVIDES,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
